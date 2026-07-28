aa2f0e6a301c350da0604a87aaedb7301ee27d7e
kondyan <arsenij.kondratenko@gmail.com>
2026-07-28 21:25:22 +0200

    added notes on qualifiers and specifiers in c

--- src/conversions.c
+++ src/conversions.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-int main() {
+int main(void) {
 
+    {
     char s[10];
     float f = 3.14159;
 
@@ -10,6 +11,84 @@ int main() {
 
     printf("String value: %s\n", s);
 
-    printf("%d %d\n", 5, '5');
+    }
+    
+    /*
+    convert string to numeric value
+    ASCII to ....
+
+    Function 	Description
+        atoi 	String to int
+        atof 	String to float
+        atol 	String to long int
+        atoll 	String to long long int
+    */
+{
+    char *pi = "3.14159";
+    float f = atof(pi);
+
+    printf("%f\n", f);
+}
+    
+
+    /*
+    for better error handling use
+    Function 	Description
+    strtol 	    String to long int
+    strtoll 	String to long long int
+    strtoul 	String to unsigned long int
+    strtoull 	String to unsigned long long int
+    strtof 	    String to float
+    strtod 	    String to double
+    strtold 	String to long double
+    */
+
+    {
+        char *s = "3490";
+
+        // null means we don't care about error information
+        // we use base 10
+        unsigned long int x = strtoul(s, NULL, 10);
+
+        printf("%lu\n", x);
+    }
+    {
+        char *s = "101010";
+
+        unsigned long int x = strtoul(s, NULL, 2);
+        printf("%lu\n", x);
+
+    }
+
+    {
+        char *s = "34x90"; // not base 10 digit
+        char *badchar;
+
+        // badchar is the first char that couldn't be converted to int
+        unsigned long int x = strtoul(s, &badchar, 10);
+
+
+        printf("%lu\n", x); // 34
+
+        
+
+        if (*badchar == '\0') {
+            printf("Success! %lu\n", x);
+        } else {
+            printf("Partial conversion: %lu\n", x);
+            printf("Invalid character: %c\n", *badchar);
+        }
+    }
+
+    {
+       printf("%d %d\n", 5, '5');
+
+       char c = '6';
+
+       int i = c - '0';
+
+       printf("%d\n", i);
+    }
+
     return 0;
 }
